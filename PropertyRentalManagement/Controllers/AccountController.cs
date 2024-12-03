3b01d62f382e8501a681bb93b4ba6f8e8260bc81
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-12-02 21:21:10 -0500

    Users from the database can login. When you logout, you are sent to the login page

--- PropertyRentalManagement/Controllers/AccountController.cs
+++ PropertyRentalManagement/Controllers/AccountController.cs
@@ -3,11 +3,20 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using PropertyRentalManagement.Models;
 using System.Security.Claims;
+using PropertyRentalManagement.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace PropertyRentalManagement.Controllers
 {
     public class AccountController : Controller
     {
+        private ApplicationDBContext _context { get; }
+
+        public AccountController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
@@ -17,46 +26,35 @@ namespace PropertyRentalManagement.Controllers
         [HttpPost]
         public IActionResult Login(LoginModel model)
         {
-            if (LoginUser(model.Email, model.Password))
-            {
-                var claims = new List<Claim>()
+            var user = _context.Users.Where(u => u.Email == model.Email && u.Password == model.Password).First();
+            if (user == null) { return View(); }
+
+            var userRole = _context.UserRoleMappings.Where(u => u.UserId == user.Id).Include(r => r.Role).First();
+
+            var claims = new List<Claim>()
                 {
-                    new Claim(ClaimTypes.Name,"Rohit Dhand")
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim("FirstName", user.FirstName),
+                    new Claim(ClaimTypes.Role, userRole.Role.Name),
                 };
 
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                var authenticationProps = new AuthenticationProperties() { };
-                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authenticationProps).Wait();
+            var authenticationProps = new AuthenticationProperties() { };
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authenticationProps).Wait();
 
-                //return Ok(new { success = true });
-                return RedirectToAction("Index", "Profile");
-            }
-            /*
-            else
-            {
-                return Ok(new { error = "Please check email or password" });
-            }
-            */
+           
+            return RedirectToAction("Index", "Profile");
 
-            return View();
+          
         }
 
-        private bool LoginUser(string email, string password)
-        {
-            if(email=="ebengaurelien@gmail.com" && password== "12345678")
-            {
-                return true;
-            }
-
-            return false;
-        }
 
         [HttpGet]
         public async Task <IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         [HttpGet]
