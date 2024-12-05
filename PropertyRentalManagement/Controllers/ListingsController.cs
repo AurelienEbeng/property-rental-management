53daa0deff25c92f3ef179a4ac328a5609c1bcb1
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2024-12-04 21:55:22 -0500

    Initial generated listing code

--- PropertyRentalManagement/Controllers/ListingsController.cs
+++ PropertyRentalManagement/Controllers/ListingsController.cs
@@ -1,88 +1,170 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PropertyRentalManagement.Context;
+using PropertyRentalManagement.Models;
 
 namespace PropertyRentalManagement.Controllers
 {
     public class ListingsController : Controller
     {
-        // GET: ListingsController
-        public ActionResult Index()
+        private readonly ApplicationDBContext _context;
+
+        public ListingsController(ApplicationDBContext context)
         {
+            _context = context;
+        }
+
+        // GET: Listings
+        public async Task<IActionResult> Index()
+        {
+            var applicationDBContext = _context.Listings.Include(l => l.Apartment).Include(l => l.Creator);
+            return View(await applicationDBContext.ToListAsync());
+        }
+
+        // GET: Listings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings
+                .Include(l => l.Apartment)
+                .Include(l => l.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // GET: Listings/Create
+        public IActionResult Create()
+        {
+            ViewData["ApartmentId"] = new SelectList(_context.Apartments, "Id", "Id");
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
-       public ActionResult AvailableApartments()
-        {
-            return View();
-        }
-
-        // GET: ListingsController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ListingsController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ListingsController/Create
+        // POST: Listings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create([Bind("Id,ApartmentId,Price,AvailableFrom,Email,CreatorId,Description,Title,DateCreated,LeaseDuration,ArePetsAllowed,IsSmokingAllowed")] Listing listing)
         {
-            try
+            if (ModelState.IsValid)
             {
+                _context.Add(listing);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewData["ApartmentId"] = new SelectList(_context.Apartments, "Id", "Id", listing.ApartmentId);
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", listing.CreatorId);
+            return View(listing);
         }
 
-        // GET: ListingsController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Listings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings.FindAsync(id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApartmentId"] = new SelectList(_context.Apartments, "Id", "Id", listing.ApartmentId);
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", listing.CreatorId);
+            return View(listing);
         }
 
-        // POST: ListingsController/Edit/5
+        // POST: Listings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ApartmentId,Price,AvailableFrom,Email,CreatorId,Description,Title,DateCreated,LeaseDuration,ArePetsAllowed,IsSmokingAllowed")] Listing listing)
         {
-            try
+            if (id != listing.Id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(listing);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ListingExists(listing.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewData["ApartmentId"] = new SelectList(_context.Apartments, "Id", "Id", listing.ApartmentId);
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", listing.CreatorId);
+            return View(listing);
         }
 
-        // GET: ListingsController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Listings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings
+                .Include(l => l.Apartment)
+                .Include(l => l.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
         }
 
-        // POST: ListingsController/Delete/5
-        [HttpPost]
+        // POST: Listings/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
+            var listing = await _context.Listings.FindAsync(id);
+            if (listing != null)
             {
-                return RedirectToAction(nameof(Index));
+                _context.Listings.Remove(listing);
             }
-            catch
-            {
-                return View();
-            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ListingExists(int id)
+        {
+            return _context.Listings.Any(e => e.Id == id);
         }
     }
 }
